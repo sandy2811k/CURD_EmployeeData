efe2e344f3e97cbf682b4867a233dafc08753cb4
RAJ <RAJ@LAPTOP-IPORQCI4>
2023-08-31 16:04:51 +0530

    Add project files.

---
+++ CURD_EmployeeData/EmployeeCURD.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURD_EmployeeData
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+    
+    public class EmployeeCURD
+    {
+        private List<Employee> employees;
+
+        public EmployeeCURD() 
+        { 
+            employees = new List<Employee>()
+            {
+                 new Employee { Id = 1, Name = "Chidanand G",Department = "HR", Salary = 60000, } ,
+                 new Employee { Id = 2, Name = "Shubham D", Department = "Finance", Salary = 65000 }
+            };        
+        }
+        //Display list
+        public List <Employee> GetEmployees()
+        {
+            return employees;
+        }
+        //Get single Employee
+        public Employee GetEmployeeById(int id)
+        {
+            //Sreach for id in the collection
+            Employee employee=new Employee();
+            foreach(Employee e in employees)
+            {
+                if(e.Id == id) //match id in the collection
+                {
+                    employee = e;
+                    break;
+                }
+            }
+            return employee;
+        }
+        //Add new Employee
+        public void AddEmpolyee(Employee e)
+        {
+            employees.Add(e);
+        }
+        //Modify the Employee
+        public void UpdateEmpolyee(Employee e)
+        {
+            foreach (Employee emp in employees)
+            {
+
+
+                if (emp.Id == e.Id)
+
+                {
+                    //item contains 
+                    emp.Id = e.Id;
+                    emp.Name = e.Name;
+                    emp.Department = e.Department;
+                    emp.Salary = e.Salary;
+                    break;
+                }
+            }
+        }
+        //Remove Product
+        public void DeleteEmployee(int id)
+        {
+            foreach(Employee emp in employees)
+            {
+                if(emp.Id==id)
+                {
+                    employees.Remove(emp);
+                    break;
+                }
+            }
+        }
+                
+
+    }
+}
---
+++ CURD_EmployeeData/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURD_EmployeeData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int no = 0;
+            EmployeeCURD curd= new EmployeeCURD();
+            do
+            {   Console.WriteLine("-------------------------");
+                Console.WriteLine("1. Employee List: ");
+                Console.WriteLine("2. Get Employee by there Id: ");
+                Console.WriteLine("3. Add Employee Details:");
+                Console.WriteLine("4. Update Employee Data:");
+                Console.WriteLine("5. Delete Empolyee Data:");
+                Console.WriteLine("Select Your Option :");
+
+                int option =Convert .ToInt32(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1: //
+                        List<Employee> list=curd.GetEmployees();
+                        Console.WriteLine("Id\t Name\t Department\t Salary\t ");
+                        foreach(Employee emp in list)
+                        {
+                            Console.WriteLine($"{emp.Id}\t {emp.Name}\t {emp.Department}\t {emp.Salary}");
+                        }
+                        break;
+
+                    case 2: // 
+                        Console.WriteLine("Enter the Employee Id:");
+                        int id=Convert .ToInt32(Console.ReadLine());
+                        Employee e=curd.GetEmployeeById(id);
+                        Console.WriteLine("Id\tName\tDepartment\tSalary\t ");
+                        Console.WriteLine($"{e.Id}  {e.Name}     {e.Department}   {e.Salary}");
+                        break;
+
+                     case 3:
+                        Employee e1 = new Employee();
+                        Console.WriteLine("Enter Employee Id:");
+                        e1.Id = Convert .ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Employee Name:");
+                        e1.Name = Console.ReadLine();
+                        Console.WriteLine("Enter department");
+                        e1.Department = Console.ReadLine();
+                        Console.WriteLine("Enter Employee Salary:");
+                        e1.Salary=Convert.ToInt32(Console.ReadLine());
+                        curd.AddEmpolyee(e1);
+                        Console.WriteLine("Employee data saved..");
+                        break; 
+                    
+                    
+                      case 4:
+                        Employee e2 = new Employee();
+                        Console.WriteLine("Enter Empolyee Id:");
+                        e2.Id=Convert .ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Employee Name:");
+                        e2.Name = Console.ReadLine();
+                        Console.WriteLine("Enter department");
+                        e2.Department = Console.ReadLine();
+                        Console.WriteLine("Enter Employee Salary:");
+                        e2.Salary = Convert.ToInt32(Console.ReadLine());
+                        curd.AddEmpolyee(e2);
+                        Console.WriteLine("Employee data updated..");
+                        break;
+                      case 5:
+                        Console.WriteLine("Enter the Employee id");
+                        int id2= Convert .ToInt32(Console.ReadLine());
+                        curd.DeleteEmployee(id2);
+                        Console.WriteLine($"{id2}Employee Data deleted");
+
+                        break;
+                }
+                Console.WriteLine("Press 1 for Continue...");
+                no =Convert .ToInt32(Console.ReadLine());
+            
+            }
+            while (no ==1);
+        }
+    }
+}
